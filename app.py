d9a2c652042ae964f65ecffa579a276a63e6aa0c
julia van albert <jv00017@mix.wvu.edu>
2026-03-30 07:43:22 -0400

    Nutrition goals now display on home page

--- app.py
+++ app.py
@@ -273,7 +273,35 @@ def register_caretaker():
 def home():
     if not session.get("user"):
         return redirect("/login")
-    return render_template("home.html", user=session["user"])
+    
+    user = session["user"]
+    
+    # Get nutrition data if user is elderly
+    nutrition_data = None
+    if user.get("role") == "elderly":
+        data = load_user_data()
+        elderly_user = None
+        for u in data.get("elderly_users", []):
+            if u["id"] == user["id"]:
+                elderly_user = u
+                break
+        
+        if elderly_user:
+            calorie_totals = track_calories(elderly_user)
+            protein_totals = track_protein(elderly_user)
+            cholesterol_totals = track_cholesterol(elderly_user)
+            sugar_totals = track_sugar(elderly_user)
+            sodium_totals = track_sodium(elderly_user)
+            
+            nutrition_data = {
+                "calories": calorie_totals,
+                "protein": protein_totals,
+                "cholesterol": cholesterol_totals,
+                "sugar": sugar_totals,
+                "sodium": sodium_totals
+            }
+    
+    return render_template("home.html", user=user, nutrition=nutrition_data)
 
 
 @app.route("/users")
@@ -407,8 +435,19 @@ def delete_food(food_id):
 
 @app.route("/nutrition")
 def nutrition():
+    if not session.get("user"):
+        return redirect("/login")
+    
     data = load_user_data()
-    user = data["elderly_users"][0]
+    user_id = session["user"]["id"]
+    user = None
+    for u in data.get("elderly_users", []) + data.get("caretaker_users", []):
+        if u["id"] == user_id:
+            user = u
+            break
+    
+    if not user or user.get("role") != "elderly":
+        return "Nutrition tracking is only available for elderly users", 403
 
     calorie_totals = track_calories(user)
     protein_totals = track_protein(user)
